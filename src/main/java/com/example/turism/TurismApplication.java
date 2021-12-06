6191f1cad20ba63a8d6c22871c8fd84e130b27bd
Silver Laas <thesilver797@gmail.com>
2021-12-06 12:47:17 +0200

    Initial commit

---
+++ src/main/java/com/example/turism/TurismApplication.java
@@ -0,0 +1,13 @@
+package com.example.turism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurismApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/turism/TurismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
